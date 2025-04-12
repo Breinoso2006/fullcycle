8b4d282a1d6a52f5626021d7243aeec1a2881cce
Bruno <breinoso2006@gmail.com>
2025-04-12 17:33:03 -0300

    Finalizando Módulo de banco de dados

--- pos-graduacao-go-expert/04_banco-de-dados/2-gorm/main.go
+++ pos-graduacao-go-expert/04_banco-de-dados/2-gorm/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type Movie struct {
@@ -10,14 +11,23 @@ type Movie struct {
 	Name       string
 	Price      float64
 	CategoryID int
-	Category   Category
-	gorm.Model // Adiciona os campos CreatedAt, UpdatedAt e DeletedAt
+	// Category   Category
+	// SerialNumber SerialNumber
+	Categories []Category `gorm:"many2many:movie_categories;"`
+	gorm.Model            // Adiciona os campos CreatedAt, UpdatedAt e DeletedAt
 }
 
 type Category struct {
-	ID   int `gorm:"primaryKey"`
-	Name string
+	ID     int `gorm:"primaryKey"`
+	Name   string
+	Movies []Movie `gorm:"many2many:movie_categories;"`
 }
+
+// type SerialNumber struct {
+// 	ID int `gorm:"primaryKey"`
+// 	Number string
+// 	MovieID int
+// }
 
 func main() {
 	dsn := "root:root@tcp(localhost:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
@@ -27,8 +37,7 @@ func main() {
 	}
 	// O migrate serve para criar a tabela no banco de dados caso ela não exista
 	// e para atualizar a tabela caso ela já exista
-	db.AutoMigrate(&Movie{})
-	db.AutoMigrate(&Category{})
+	db.AutoMigrate(&Movie{}, &Category{})
 
 	// Comandos para inserir dados no banco de dados
 
@@ -76,17 +85,42 @@ func main() {
 
 	// Relacionamento entre tabelas
 
-	// category := Category{Name: "Aventura"}
-	// db.Create(&category)
-	// product := Movie{Name: "Uncharted", Price: 10.99, CategoryID: category.ID}
-	// db.Create(&product)
+	category := Category{Name: "Loucura"}
+	db.Create(&category)
+	movie := Movie{Name: "Panelaco", Price: 13.99, CategoryID: category.ID}
+	db.Create(&movie)
+	// serialNumber := SerialNumber{Number: "12344", MovieID: movie.ID}
+	// db.Create(&serialNumber)
 
-	var movies []Movie
+	// var movies []Movie
 	// Preload é usado para carregar os dados relacionados de uma tabela
-	db.Preload("Category").Find(&movies)
-	for _, movie := range movies {
-		println(movie.Name, movie.Category.Name)
+	// db.Preload("Category").Find(&movies)
+	// for _, movie := range movies {
+	// 	println(movie.Name, movie.Category.Name)
+	// }
+
+	var categories []Category
+	// Preload é usado para carregar os dados relacionados de uma tabela
+	// Pode ser necessário usar algo como Movies.SerialNumber dentro do Preload
+	err = db.Model(&Category{}).Preload("Movies").Find(&categories).Error
+	if err != nil {
+		panic(err)
+	}
+	for _, category := range categories {
+		println(category.Name, ":")
+		for _, movie := range category.Movies {
+			println("-", movie.Name)
+		}
 	}
 
-
+	// Lockando a linha para evitar que outra transação a altere
+	tx := db.Begin()
+	var c Category
+	err = tx.Debug().Clauses(clause.Locking{Strength: "UPDATE"}).First(&c, 1).Error
+	if err != nil {
+		panic(err)
+	}
+	c.Name = "Terror"
+	tx.Debug().Save(&c)
+	tx.Commit()
 }
