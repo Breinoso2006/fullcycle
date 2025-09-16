92e0c7b98ae1e26d2cad42798400b4d48972bbc4
Bruno <breinoso2006@gmail.com>
2025-09-16 18:22:49 -0300

    finalizando módulo extras

---
+++ pos-graduacao-go-expert/22-extras/1-graceful-shutdown/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	server := &http.Server{Addr: ":8080"}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(4 * time.Second)
+		w.Write([]byte("Hello, World!"))
+	})
+
+	go func() {
+		fmt.Println("Server is running on port 8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not listen on :8080: %v\n", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	fmt.Println("Shutting down server...")
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+	fmt.Println("Server exiting")
+}
