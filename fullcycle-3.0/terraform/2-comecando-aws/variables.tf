80a84d4a02b2dfbb152842850e49c637d0ac9dfc
Bruno <breinoso2006@gmail.com>
2025-12-07 19:59:52 -0300

    continuando módulo terraform

---
+++ fullcycle-3.0/terraform/2-comecando-aws/variables.tf
@@ -0,0 +1,29 @@
+variable "prefix" {
+  description = "Prefixo para os recursos"
+  type        = string
+  default     = "fullcycle"
+}
+
+variable "retention_days" {
+  description = "Número de dias para retenção dos logs"
+  type        = number
+  default     = 7
+}
+
+variable "node_desired_size" {
+  description = "Número desejado de nós no cluster"
+  type        = number
+  default     = 1
+}
+
+variable "node_max_size" {
+  description = "Número máximo de nós no cluster"
+  type        = number
+  default     = 2
+}
+
+variable "node_min_size" {
+  description = "Número mínimo de nós no cluster"
+  type        = number
+  default     = 1
+}
